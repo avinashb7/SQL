6aeeb6571cc259ac28f36d70239e22e67f50c0e3
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-12-10 12:26:36 +0530

    Update nthHighestSalary.sql

--- nthHighestSalary.sql
+++ nthHighestSalary.sql
@@ -2,3 +2,11 @@
 
 /* Find second highest salary. */
 SELECT name, MAX(salary) AS salary FROM employee WHERE salary < (SELECT MAX(salary) FROM employee);
+
+/* Using DENSE_RANK() */
+SELECT salary,name
+FROM
+(
+SELECT salary,name,DENSE_RANK() OVER(ORDER BY salary DESC) Rno from employee
+) tbl
+WHERE Rno=3
