a6c06d85b49dbcf51f881e0f13f308cd1fd748f0
Avinash <57484520+avinashb7@users.noreply.github.com>
2019-12-10 12:24:18 +0530

    Update nthHighestSalary.sql

--- nthHighestSalary.sql
+++ nthHighestSalary.sql
@@ -1,3 +1,4 @@
-# Find the nth Highest salary in a EMPLOYEE table
+// Find the nth Highest salary in a EMPLOYEE table
 
+// Find second highest salary.
 SELECT name, MAX(salary) AS salary FROM employee WHERE salary < (SELECT MAX(salary) FROM employee);
